600ec82744b05ab9630a7669021f560d90c5d0e3
Sam <sahe2032@colorado.edu>
2021-04-12 10:40:41 -0600

    added images and layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
+// const path = require("path");
+// module.exports = {
+//   mode: "development",
+//   entry: "./src/index.js",
+//   output: {
+//     filename: "main.js",
+//     path: path.resolve(__dirname, "dist")
+//   },
+//   module: {
+//     rules: [{
+//       test: /\.scss$/,
+//
+//       use: [
+//         "style-loader", // 3. injects css into DOM
+//         "css-loader", // 2.turns css into commonjs
+//         "sass-loader", // 1.turns sass into css
+//       ],
+//     }, ],
+//   },
+// };
+
+
 const path = require("path");
 module.exports = {
   mode: "development",
@@ -8,13 +30,23 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.scss$/,
-
-      use: [
-        "style-loader", // 3. injects css into DOM
-        "css-loader", // 2.turns css into commonjs
-        "sass-loader", // 1.turns sass into css
-      ],
-    }, ],
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. injects css into DOM
+          "css-loader", // 2.turns css into commonjs
+          "sass-loader", // 1.turns sass into css
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: "url-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [{
+          loader: 'file-loader',
+        }, ],
+      },
+    ],
   },
 };
